90b9e102b0d2a9391374344cee2e1d6fa933a341
Matt Patterson <matt@reprocessed.org>
2011-06-30 14:59:20 +0100

    Add default host to actionmailer config for production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -46,4 +46,6 @@ Halberd::Application.configure do
 
   # Send deprecation notices to registered listeners
   config.active_support.deprecation = :notify
+
+  config.action_mailer.default_url_options = { :host => 'signonotron.alpha.gov.uk' }
 end
