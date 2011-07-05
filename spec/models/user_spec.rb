3b86e6bc2dfa90f2732e20917f29fa150abf7e34
Matt Patterson <matt@reprocessed.org>
2011-07-05 15:51:01 +0100

    Shamefacedly bringing specs into line with code

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,11 +3,11 @@ require 'spec_helper'
 describe User do
   describe "JSON serialisation" do
     before(:each) do
-      @user = User.new(:name => 'Matt', :email => 'matt@alphagov.co.uk', :github => 'fidothe', :twitter => 'fidothe')
+      @user = User.new(:name => 'Matt', :email => 'matt@alphagov.co.uk', :github => 'fidothe', :twitter => 'fidothe', :uid => 'abcde', :version => 1)
     end
     
     it "serialises a sensible set of attributes to JSON" do
-      JSON.parse(@user.to_sensible_json).should == {'user' => {'name' => 'Matt', 'email' => 'matt@alphagov.co.uk', 'github' => 'fidothe', 'twitter' => 'fidothe'}}
+      JSON.parse(@user.to_sensible_json).should == {'user' => {'name' => 'Matt', 'email' => 'matt@alphagov.co.uk', 'github' => 'fidothe', 'twitter' => 'fidothe', 'uid' => 'abcde', 'version' => 1}}
     end
   end
 end
