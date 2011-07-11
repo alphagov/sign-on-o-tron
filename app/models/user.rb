c341b27a2cbf1e4a3ba2ed4f6316f7e6f510e445
Matt Patterson <matt@reprocessed.org>
2011-07-11 16:43:06 +0100

    Add more useful info to the views; remove some noise
    * Added gravatar and name so you know it's you when you're logged in
    * Added sign-out
    * add name of requesting OAuth client to new authorisation view
    * Add list of authorised apps to user view

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,23 @@
+require 'digest/md5'
+require 'rack/utils'
+require "digest"
+
 class User < ActiveRecord::Base
   devise :database_authenticatable, :recoverable, :rememberable, :trackable, :validatable, :timeoutable
   
   attr_accessible :uid, :name, :email, :password, :password_confirmation, :twitter, :github, :beard
   attr_readonly   :uid
-  
+
+  has_many :authorisations, :as => :resource_owner, :class_name => "OAuth2::Provider::Models::ActiveRecord::Authorization"
+
+  def gravatar_url(opts = {})
+    opts.symbolize_keys!
+    qs = opts.select { |k, v| k == :s }.collect { |k, v| "#{k}=#{Rack::Utils.escape(v)}" }.join("&")
+    qs = "?" + qs unless qs == ""
+    scheme_and_host = opts[:ssl] ? "https://secure.gravatar.com" : "http://www.gravatar.com"
+    "#{scheme_and_host}/avatar/#{Digest::MD5.hexdigest(email.downcase)}#{qs}"
+  end
+
   def to_sensible_json
     to_json(:only => [:uid, :version, :name, :email, :github, :twitter])
   end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -5,9 +5,23 @@ describe User do
     before(:each) do
       @user = User.new(:name => 'Matt', :email => 'matt@alphagov.co.uk', :github => 'fidothe', :twitter => 'fidothe', :uid => 'abcde', :version => 1)
     end
-    
+
     it "serialises a sensible set of attributes to JSON" do
       JSON.parse(@user.to_sensible_json).should == {'user' => {'name' => 'Matt', 'email' => 'matt@alphagov.co.uk', 'github' => 'fidothe', 'twitter' => 'fidothe', 'uid' => 'abcde', 'version' => 1}}
+    end
+
+    describe "generating gravatar URLs" do
+      it "generates a valid default-sized URL" do
+        @user.gravatar_url.should == "http://www.gravatar.com/avatar/7576330ca22fa087275c3ba1677ec1d5"
+      end
+
+      it "can generate a URL for a different-sized gravatar" do
+        @user.gravatar_url(:s => 128).should == "http://www.gravatar.com/avatar/7576330ca22fa087275c3ba1677ec1d5?s=128"
+      end
+
+      it "can generate secure gravatar URLs if needed" do
+        @user.gravatar_url(:ssl => true).should == "https://secure.gravatar.com/avatar/7576330ca22fa087275c3ba1677ec1d5"
+      end
     end
   end
 end
