7f2c5fc62e4f308e11db7f5013916d945017a547
James Stewart <jys@ketlai.co.uk>
2011-08-15 13:26:33 +0100

    Try to reduce duplicate authorisations required when signing in

--- app/controllers/authorisations_controller.rb
+++ app/controllers/authorisations_controller.rb
@@ -5,7 +5,12 @@ class AuthorisationsController < ApplicationController
   before_filter :block_invalid_authorization_code_requests
 
   def new
-    @client = oauth2_authorization_request.client
+    existing_authorisations = client.authorizations.where(:resource_owner => current_user, :scope => params['scope']).where('expires_at > ?', DateTime.now)
+    if existing_authorisations.any?
+      throw_response Responses.redirect_with_code(existing_authorisations.first.code, redirect_uri)
+    else
+      @client = oauth2_authorization_request.client
+    end
   end
 
   def create
