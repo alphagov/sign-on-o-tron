7ef158dca3fbb4d0fe43ea45e4e448ba46fb9e6c
Craig R Webster <craig@barkingiguana.com>
2011-10-21 17:15:04 +0100

    The latest OAuth2 provider uses the header

--- lib/warden_oauth_provider_strategy.rb
+++ lib/warden_oauth_provider_strategy.rb
@@ -1,8 +1,26 @@
 require 'warden/strategies/base'
 
 class WardenOAuthProviderStrategy < Warden::Strategies::Base
+  def token
+    token_from_param || token_from_header
+  end
+
+  def token_from_param
+    params["oauth_token"]
+  end
+
+  def token_from_header
+    if authorization_header.provided?
+      authorization_header.params
+    end
+  end
+
+  def authorization_header
+    @authorization_header ||= Rack::Auth::AbstractRequest.new(env)
+  end
+
   def valid?
-    params['oauth_token']
+    token.present?
   end
 
   def authenticate!
