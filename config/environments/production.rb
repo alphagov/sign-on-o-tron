0b073d7798a3fb3fd319f7a56101890acfbbf966
Paul Battley <pbattley@gmail.com>
2011-10-24 12:21:02 +0100

    Set correct email host for production.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -47,5 +47,5 @@ Halberd::Application.configure do
   # Send deprecation notices to registered listeners
   config.active_support.deprecation = :notify
 
-  config.action_mailer.default_url_options = { :host => 'signonotron.alpha.gov.uk' }
+  config.action_mailer.default_url_options = { :host => 'signonotron.production.alphagov.co.uk' }
 end
